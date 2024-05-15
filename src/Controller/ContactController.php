5491f98f8a36ede232aa034b8088db71e332d781
florian78370 <florian.forget2004@orange.fr>
2024-05-15 17:14:12 +0200

    partie11

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contacts", name="app_contacts", methods={"GET"})
+     */
+    public function listeContacts(ContactRepository $repo)
+    {
+        
+        $Contacts=$repo->findAll();
+        return $this->render('contact/listeContacts.html.twig',[
+            'lesContacts' => $Contacts
+        ]);
+            
+        
+    }
+    /**
+     * @Route("/contact/{id}", name="fichecontacts", methods={"GET"})
+     */
+    public function ficheContacts($id, ContactRepository $repo)
+    {
+        
+        $Contact=$repo->find($id);
+        return $this->render('contact/ficheContact.html.twig',[
+            'leContact' => $Contact
+        ]);
+            
+        
+    }
+}
